befc90a14cecc818eb8234c4d4035c569d0f270f
Phoomparin Mano <phoomparin@gmail.com>
2024-12-14 16:42:04 +0800

    nuxt conf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: {enabled: true},
   modules: ['@nuxtjs/tailwindcss'],
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
 })
