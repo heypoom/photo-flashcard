1fb31c62a7995840323abbdcddfba768a75617b1
Phoomparin Mano <phoomparin@gmail.com>
2025-01-01 03:00:34 +0700

    add cursor-pointer to camera button

--- components/AddPhoto.vue
+++ components/AddPhoto.vue
@@ -57,7 +57,7 @@
       <input id="file-upload" type="file" v-on:change="uploadAndPredict" class="hidden" :disabled="uploadingRef" />
 
       <label for="file-upload">
-        <div class="bg-red-500 text-white p-3 rounded-full shadow-xl" :class="{ 'opacity-30 animate-spin': uploadingRef }">
+        <div class="bg-red-500 text-white p-3 rounded-full shadow-xl cursor-pointer" :class="{ 'opacity-30 animate-spin': uploadingRef }">
           <Icon :icon="uploadingRef ? 'solar:clock-circle-outline' : 'solar:camera-bold'" class="text-3xl" />
         </div>
       </label>
