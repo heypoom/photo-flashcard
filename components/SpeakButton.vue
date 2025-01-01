40780076dc98604a1ba97c9873e40caafbd2f42f
Phoomparin Mano <phoomparin@gmail.com>
2025-01-01 14:24:52 +0700

    configure better speech synthesizer

--- components/SpeakButton.vue
+++ components/SpeakButton.vue
@@ -2,6 +2,7 @@
   import {Icon} from '@iconify/vue'
 
   import {speak} from "../utils/synthesis"
+  import {speakWithPolly} from "../utils/polly"
 
   const props = defineProps<{
     word?: string
@@ -12,6 +13,6 @@
   <Icon
     icon="solar:user-speak-bold"
     class="text-[33px] cursor-pointer text-white bg-gray-700 hover:bg-gray-800 p-[6px] rounded-full"
-    @click="() => { props.word && speak({message: props.word}) }"
+    @click="() => { props.word && speakWithPolly(props.word) }"
   />
 </template>
---
+++ utils/polly.ts
@@ -0,0 +1,102 @@
+import {PollyClient, SynthesizeSpeechCommand} from '@aws-sdk/client-polly'
+
+import {
+  CognitoIdentityClient,
+  GetIdCommand,
+  GetCredentialsForIdentityCommand,
+} from '@aws-sdk/client-cognito-identity'
+
+const REGION = 'ap-southeast-1'
+const IDENTITY_POOL_ID = 'ap-southeast-1:809ab693-ab94-490b-8b62-9d44589ebbf7'
+
+let credentials:
+  | {
+      accessKeyId?: string
+      secretAccessKey?: string
+      sessionToken?: string
+      expiration?: Date
+    }
+  | undefined
+
+async function getGuestCredentials() {
+  const client = new CognitoIdentityClient({region: REGION})
+
+  const {IdentityId} = await client.send(
+    new GetIdCommand({
+      IdentityPoolId: IDENTITY_POOL_ID,
+    })
+  )
+
+  const {Credentials} = await client.send(
+    new GetCredentialsForIdentityCommand({
+      IdentityId,
+    })
+  )
+
+  if (!Credentials) {
+    return
+  }
+
+  return {
+    accessKeyId: Credentials.AccessKeyId,
+    secretAccessKey: Credentials.SecretKey,
+    sessionToken: Credentials.SessionToken,
+    expiration: Credentials.Expiration,
+  }
+}
+
+export async function speakWithPolly(message: string) {
+  if (
+    !credentials ||
+    (credentials.expiration && credentials.expiration < new Date())
+  ) {
+    credentials = await getGuestCredentials()
+    console.log('Guest credentials obtained:', credentials)
+  }
+
+  if (!credentials) {
+    return
+  }
+
+  const pollyClient = new PollyClient({
+    region: REGION,
+    credentials: {
+      accessKeyId: credentials.accessKeyId ?? '',
+      secretAccessKey: credentials.secretAccessKey ?? '',
+      sessionToken: credentials.sessionToken,
+    },
+  })
+
+  try {
+    const command = new SynthesizeSpeechCommand({
+      Engine: 'neural',
+      LanguageCode: 'cmn-CN',
+      OutputFormat: 'mp3',
+      Text: message,
+      TextType: 'text',
+      VoiceId: 'Zhiyu',
+    })
+
+    const {AudioStream} = await pollyClient.send(command)
+
+    if (!AudioStream) {
+      return
+    }
+
+    // Convert AudioStream to blob
+    const blob = new Blob([await AudioStream.transformToByteArray()], {
+      type: 'audio/mpeg',
+    })
+
+    const url = URL.createObjectURL(blob)
+    const audio = new Audio(url)
+
+    audio.onended = () => URL.revokeObjectURL(url)
+    audio.play()
+
+    return audio
+  } catch (error) {
+    console.error('Error synthesizing speech:', error)
+    throw error
+  }
+}
