8f0f154796b236a38211e0c3064dc6a4d5a55ea1
Phoomparin Mano <phoomparin@gmail.com>
2025-01-01 12:57:07 +0700

    update button size

--- components/SpeakButton.vue
+++ components/SpeakButton.vue
@@ -11,7 +11,7 @@
 <template>
   <Icon
     icon="solar:user-speak-bold"
-    class="text-3xl cursor-pointer text-white bg-gray-700 hover:bg-gray-800 p-[6px] rounded-full"
+    class="text-[33px] cursor-pointer text-white bg-gray-700 hover:bg-gray-800 p-[6px] rounded-full"
     @click="() => { props.word && speak({message: props.word}) }"
   />
 </template>
