742cf59dfe72484c81660ccd460b2e51769b3774
Phoomparin Mano <phoomparin@gmail.com>
2025-01-01 20:09:21 +0700

    use updated at for sort order

--- server/api/words.ts
+++ server/api/words.ts
@@ -1,20 +1,30 @@
 import {getGristApi} from '~/utils/grist'
 
+type WordRecord = {
+  manualSort: number
+  Photos: number[]
+  UpdatedAt: number
+}
+
 export default defineEventHandler(async () => {
   const grist = getGristApi()
 
   const words = await grist.fetchTable('Words')
   const photos = await grist.fetchTable('Photos')
 
-  return words.map((word) => {
-    const photoRefId = (word.Photos as number[])?.[1]
-    const photo = photos.find((photo) => photo.id === photoRefId)
-    const attachmentId = (photo?.Photo as number[])?.[1]
+  return words
+    .map((record) => {
+      const word = record as unknown as WordRecord
 
-    return {
-      ...word,
-      photoRefId,
-      attachmentId,
-    }
-  })
+      const photoRefId = word.Photos?.[1]
+      const photo = photos.find((photo) => photo.id === photoRefId)
+      const attachmentId = (photo?.Photo as number[])?.[1]
+
+      return {
+        ...word,
+        photoRefId,
+        attachmentId,
+      }
+    })
+    .sort((a, b) => b.UpdatedAt - a.UpdatedAt)
 })
