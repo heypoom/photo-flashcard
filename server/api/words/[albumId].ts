2874e8640b47633784ae6b73a872314ccedffbce
Phoomparin Mano <phoomparin@gmail.com>
2025-01-02 20:20:52 +0700

    fix photo endpoint

--- server/api/words/[albumId].ts
+++ server/api/words/[albumId].ts
@@ -7,18 +7,27 @@ type WordRecord = {
   Albums: number
 }
 
-export default defineEventHandler(async (event) => {
-  const albumId = getRouterParam(event, "albumId")
-  const grist = getGristApi()
+type PhotoRecord = {
+  id: number
+  Photo: number[]
+}
 
-  const words = await grist.fetchTable("Words")
-  const photos = await grist.fetchTable("Photos")
+export default defineEventHandler(async (event) => {
+  const start = Date.now()
+  const grist = getGristApi()
+  const albumId = getRouterParam(event, "albumId")
+  const filter = { Albums: [Number(albumId)] }
+
+  const [words, photos] = await Promise.all([
+    grist.fetchTable("Words", filter),
+    grist.fetchTable("Photos", filter),
+  ])
+
+  console.log(`[/api/words/${albumId}] took ${Date.now() - start}ms`)
 
   return words
-    .filter((word) => word.Albums === Number(albumId))
     .map((record) => {
       const word = record as unknown as WordRecord
-
       const photoRefId = word.Photos?.[1]
       const photo = photos.find((photo) => photo.id === photoRefId)
       const attachmentId = (photo?.Photo as number[])?.[1]
